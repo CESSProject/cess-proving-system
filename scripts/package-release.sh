9afd6d0df65c3f2c7eccc50586df72093068da9b
Teh Sunn <tehsunnliu@gmail.com>
2021-11-22 11:16:17 +0530

    Reset Versions

--- scripts/package-release.sh
+++ scripts/package-release.sh
@@ -3,18 +3,18 @@
 set -Eeuo pipefail
 
 if [ -z "$1" ]; then
-  TAR_FILE=`mktemp`.tar.gz
+  TAR_FILE=$(mktemp).tar.gz
 else
   TAR_FILE=$1
 fi
 
-TAR_PATH=`mktemp -d`
+TAR_PATH=$(mktemp -d)
 
 mkdir -p $TAR_PATH
 mkdir -p $TAR_PATH/bin
 mkdir -p $TAR_PATH/misc
 
-cp filecoin-proofs/parameters.json $TAR_PATH/misc/
+cp cess-proofs/parameters.json $TAR_PATH/misc/
 cp target/release/paramcache $TAR_PATH/bin/
 cp target/release/paramfetch $TAR_PATH/bin/
 
